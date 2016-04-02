2d24cd7e3f258fb164c364490ba1b6b4d8cfeb1b
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2016-04-02 12:40:37 -0400

    Fixed aliase naming

--- TimesSquareMexaPizaNeseFinder/Helpers/SODAHelper.cs
+++ TimesSquareMexaPizaNeseFinder/Helpers/SODAHelper.cs
@@ -7,16 +7,27 @@ namespace TimesSquareMexaPizaNeseFinder.Helpers
 {
     public class SODAHelper
     {
-        private const string _AppToken = "";
+        private const string _AppToken = "RDy1uO96cbxAOMsJ6IpZqcEws";
         private const string _APIEndPointHost = "data.cityofnewyork.us";
         private const string _APIEndPoint4x4 = "7rwn-iw5c";
+
+        public static string GetMeta()
+        {
+            //Create client to talk to OpenDat API Endpoint
+            var client = new SodaClient(_APIEndPointHost, _AppToken);
+
+            //read metadata of a dataset using the resource identifier (Socrata 4x4)
+            var metadata = client.GetMetadata(_APIEndPoint4x4);
+
+            return string.Format("{0} has {1} views. Last updated on {2:g}", metadata.Name, metadata.ViewsCount, metadata.RowsLastUpdated);
+        }
 
         public static PagedList<BusinessLocation> GetBusinessLocations(string SearchQuery, int PageNumber, int PageSize, string OrderBy, bool OrderByAscDesc)
         {
             var client = new SodaClient(_APIEndPointHost, _AppToken);
             var dataset = client.GetResource<PagedList<BusinessLocation>>(_APIEndPoint4x4);
             var columns = new[] { "company_name", "sub_subindustry", "phone", " location_1" };
-            var aliases = new[] { "CompanyName", "SubIndustry", "Phone", "Location" };
+            var aliases = new[] { "CompanyName", "SubIndustry", "fone", "Location" };
             var soql = new SoqlQuery().Select(columns)
                 .As(aliases)
                 .Order((OrderByAscDesc) ? SoqlOrderDirection.ASC : SoqlOrderDirection.DESC, new[] { OrderBy });
